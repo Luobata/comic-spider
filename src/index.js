679c2815cfba6874ef087a3d4e20f035b6a22e4b
Luobata <602975805@qq.com>
2018-01-02 01:13:02 +0800

    init;
    Signed-off-by: Luobata <602975805@qq.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://comic.kukudm.com/comiclist/1694/index.htm');
-  await page.screenshot({path: 'example.png'});
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://comic.kukudm.com/comiclist/1694/index.htm', {
+        waitUntil: 'domcontentloaded',
+        timeout: 3000000
+    });
 
-  await browser.close();
+    await(2000);
+    let aTag = await page.evaluate(() => {
+        let as = [...document.querySelectorAll('#comiclistn dd a')].filter(v => v.innerHTML.indexOf('食戟之灵') !== -1);
+        return as.map((a) => {
+            return {
+                href: a.href.trim(),
+                //text: a.text.match(/.*?(\d+)/)[1]
+                text: a.text
+            }
+        });
+    });
+
+    const i = aTag[0];
+    //for (let i of aTag) {
+    const name = i.text;
+    const base = i.href.replace(/\d+\.htm/, '');
+    const imgs = [];
+    await page.goto(i.href, {
+        waitUntil: 'domcontentloaded',
+        timeout: 3000000
+    });
+    await(2000);
+    let totalNum = 0;;
+    let currentNum = 0;
+    await page.evaluate(() => {
+        let num = document.querySelectorAll('table tr td')[1].innerText.match(/共(\d+)页/)[1];
+        totalNum = parseInt(num, 10) | 0;
+    });
+
+    for (let i = currentNum + 1; i < totalNum; i++) {
+        const link = `${base}${i}.htm`;
+        await page.goto(link, {
+            waitUntil: 'domcontentloaded',
+            timeout: 3000000
+        });
+        await(2000);
+        const img = await page.evaluate(() => {
+            return document.querySelector('img');
+        });
+        imgs.push(img);
+    }
+    console.log(imgs);
+    //}
+
+    await browser.close();
 })();
