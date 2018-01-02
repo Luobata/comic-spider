82b3fc754c65de222382d2870265d50e8e3e7e30
yaochenghao <yaochenghao@bytedance.com>
2018-01-02 22:02:32 +0800

    init

--- src/index.js
+++ src/index.js
@@ -21,47 +21,46 @@ const fs = require('fs');
         });
     });
 
-    const i = aTag[0];
-    //for (let i of aTag) {
-    const name = i.text;
-    const base = i.href.replace(/\d+\.htm/, '');
-    const imgs = [];
-    await page.goto(i.href, {
-        waitUntil: 'domcontentloaded',
-        timeout: 3000000
-    });
-    //let totalNum = 0;;
-    let currentNum = 0;
-    let totalNum = await page.evaluate(() => {
-        let num = document.querySelectorAll('table tr td')[1].innerText.match(/共(\d+)页/)[1];
-        console.log(num);
-        return num;
-        //totalNum = parseInt(num, 10) | 0;
-    });
-    console.log(totalNum);
-
-    for (let i = currentNum + 1; i < totalNum; i++) {
-        const link = `${base}${i}.htm`;
-        console.log(link);
-        await page.goto(link, {
+    //const i = aTag[0];
+    for (let i of aTag) {
+        const name = i.text;
+        const base = i.href.replace(/\d+\.htm/, '');
+        const imgs = [];
+        await page.goto(i.href, {
             waitUntil: 'domcontentloaded',
             timeout: 3000000
         });
-        await(2000);
-        const img = await page.evaluate(() => {
-            return document.querySelector('img').src;
+        //let totalNum = 0;;
+        let currentNum = 0;
+        let totalNum = await page.evaluate(() => {
+            let num = document.querySelectorAll('table tr td')[1].innerText.match(/共(\d+)页/)[1];
+            return num;
+            //totalNum = parseInt(num, 10) | 0;
         });
-        imgs.push(img);
+        console.log(name, totalNum);
+
+        for (let i = currentNum + 1; i < totalNum; i++) {
+            const link = `${base}${i}.htm`;
+            //console.log(link);
+            await page.goto(link, {
+                waitUntil: 'domcontentloaded',
+                timeout: 3000000
+            });
+            await(2000);
+            const img = await page.evaluate(() => {
+                return document.querySelector('img').src;
+            });
+            imgs.push(img);
+        }
+        const compiled = pug.compileFile('src/tpl/tpl.pug', {
+            pretty: true,
+        });
+        const re = compiled({
+            data: imgs,
+        });
+        fs.writeFileSync(`src/data/${name}.html`, re);
+        //console.log(imgs);
     }
-    const compiled = pug.compileFile('src/tpl/tpl.pug', {
-        pretty: true,
-    });
-    const re = compiled({
-        data: imgs,
-    });
-    fs.writeFileSync(`src/data/${name}.html`, re);
-    console.log(imgs);
-    //}
 
     await browser.close();
 })();
